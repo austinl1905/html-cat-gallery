0c18a0b86113c8102cd29da04af3f1fa64b602a7
Austin Liu <austinl1905@hstat.org>
2023-02-15 15:04:25 +0000

    small tweaks to card 11

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const spoilerImage = document.getElementById("spoiler-image");
+const spoilerText = document.getElementById("spoiler-text");
 spoilerImage.addEventListener("click", clickFunction);
 
 function clickFunction() {
     spoilerImage.style.filter = "blur(0rem)";
+    spoilerText.style.display = "none";
 }
